387d88136e9b428592c4bc8703d3d37261cfdd17
kazuyoshi.kawakami <saruyama.monki@gmail.com>
2013-06-09 13:30:54 +0900

    firstCommit
    Xcode project作成

---
+++ ProgKitchenTImer/ProgKitchenTImer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ProgKitchenTImer
+//
+//  Created by kazuyoshi kawakami on 13/06/09.
+//  Copyright (c) 2013年 kazuyoshi kawakami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ProgKitchenTImer/ProgKitchenTImer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ProgKitchenTImer
+//
+//  Created by kazuyoshi kawakami on 13/06/09.
+//  Copyright (c) 2013年 kazuyoshi kawakami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
